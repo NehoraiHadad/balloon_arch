48dd4f367f1a8e2558d8afae6f210f5839b96167
Nehorai Hadad <Nehorai.hadad@gmail.com>
2023-02-15 09:37:37 +0200

    Deploy Balloon-arch to GitHub Pages

---
+++ precache-manifest.d68a5a5c6f47b6c3f65bd9533a6a01e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6695fcaea69f4a48d103",
+    "url": "/balloon_arch/static/css/main.2ae5cbb4.chunk.css"
+  },
+  {
+    "revision": "6695fcaea69f4a48d103",
+    "url": "/balloon_arch/static/js/main.6695fcae.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/balloon_arch/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/balloon_arch/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "e359f1647a25904c0bdc",
+    "url": "/balloon_arch/static/js/runtime~main.e359f164.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/balloon_arch/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "111c74efd9d492cf77136db453ff37a9",
+    "url": "/balloon_arch/index.html"
+  }
+];
