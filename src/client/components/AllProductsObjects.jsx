2e6481d8a4ed36bf8bdc4be20a4233d7d23b6560
Nehorai Hadad <Nehorai.hadad@gmail.com>
2023-02-27 10:05:56 +0200

    update WhatsappButton

--- src/client/components/AllProductsObjects.jsx
+++ src/client/components/AllProductsObjects.jsx
@@ -7,56 +7,56 @@ const AllProductObjects =
             id:1,
             image: Images.product1,
             title: "קשת בלונים ענקית",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 1500
         } ,
         {
             id:2,
             image: Images.product2,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:3,
             image: Images.product3,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:4,
             image: Images.product4,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:5,
             image: Images.product5,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:6,
             image: Images.product6,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:7,
             image: Images.product7,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 300
         },
         {
             id:8,
             image: Images.product8,
-            title: "Lizard",
-            description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+            title: "סתם טקסט ",
+            description: "כמה פרטים לא חשובים שאתם לא צריכים לדעת אבל בגלל שהם כתובים בשפה יפה ומקצועית זה מגניב",
             price: 20
         }
     ]
--- src/client/components/Cart/WhatsappButton.jsx
+++ src/client/components/Cart/WhatsappButton.jsx
@@ -1,61 +1,52 @@
 import {Button} from '@mui/material'
 import { Link } from 'react-router-dom';
 
-// const cartDetails = [];
-
-//   cartItems.forEach(item => {
-//     const { title, price, quantity } = item;
-//     const itemDetails = `${title} - $${price.toFixed(2)} x ${quantity}\n`;
-//     cartDetails.push(itemDetails);
-//   });
-
-//   return cartDetails;
-// }
-
-
 const WhatsappButton = ({cartItems, totalCost}) => {
     
     function getCartDetails(cartItems , totalCost) {
         const cartDetails = [];
-      
+
         // Calculate the maximum length of the title, price, and quantity fields
         const titleLength = Math.max(...cartItems.map(item => item.title.length));
         const priceLength = Math.max(...cartItems.map(item => `$${item.price.toFixed(2)}`.length));
         const quantityLength = Math.max(...cartItems.map(item => item.quantity.toString().length));
 
         // Add the header row to the cartDetails array
-        const headerRow = `| ${'Title'.padEnd(titleLength)} | ${'Price'.padEnd(priceLength)} | ${'Quantity'.padEnd(quantityLength)} |\n`;
+        const headerRow = `| ${'שם'.padEnd(titleLength + titleLength/2)} | ${'מחיר'.padEnd(priceLength + priceLength/2)} | ${'כמות'.padEnd(quantityLength + quantityLength/2)} |%0a`;
         cartDetails.push(headerRow);
 
         // Add a separator row to the cartDetails array
-        const separatorRow = `+${'-'.repeat(titleLength + 2)}+${'-'.repeat(priceLength + 2)}+${'-'.repeat(quantityLength + 2)}+\n`;
+        const separatorRow = `${'-'.repeat(titleLength + titleLength + priceLength)}%0a`;
         cartDetails.push(separatorRow);
 
         // Add each item row to the cartDetails array
         cartItems.forEach(item => {
             const { title, price, quantity } = item;
-            const itemRow = `| ${title.padEnd(titleLength)} | $${price.toFixed(2).padEnd(priceLength)} | ${quantity.toString().padEnd(quantityLength)} |\n`;
+            const itemRow = `| ${title.padEnd(titleLength)} | ₪${price.toFixed(2).padEnd(priceLength)} | ${quantity.toString().padEnd(quantityLength)} |%0a`;
             cartDetails.push(itemRow);
-    });
+        });
 
         // Add a final separator row to the cartDetails array
         cartDetails.push(separatorRow);
+        cartDetails.push(`המחיר הסופי הוא : ₪${totalCost}`);
 
-        return cartDetails;
 
-      }
-      
+        // Construct the final message string by joining the cartDetails array
+        const message =`רשימת הפריטים בעגלת הקניות שלך: %0a%0a ${cartDetails}`;
 
+        return message; 
+    }
+    
     return(
-    <Link  
-        to={`https://wa.me/0547401660?text=${getCartDetails(cartItems , totalCost)}`} 
-        target='_blank'
-        style={{ textDecoration: 'none' }}> 
-        <Button variant="contained" color="primary" disableElevation>
-            לתשלום
-        </Button>
-    </Link>
-        )
+        <Link  
+            to={`https://wa.me/+972547401660?text=${getCartDetails(cartItems , totalCost)}`} 
+            target='_blank'
+            style={{ textDecoration: 'none' }}> 
+            <Button variant="contained" color="primary" disableElevation>
+                לתשלום
+            </Button>
+        </Link>
+    )
 }
 
 export default WhatsappButton;
