f1b558461c3c1062a6a45453033636bfb385210d
Nehorai Hadad <Nehorai.Hadad@gmail.com>
2023-03-08 09:26:54 +0200

    fix Whatsapp button MEHADRIN data

--- src/client/components/Cart/WhatsappButton.jsx
+++ src/client/components/Cart/WhatsappButton.jsx
@@ -41,7 +41,7 @@ const WhatsappButton = ({cartItems, totalCost, isMehadrin}) => {
       
     return(
         <Link  
-            to={`https://wa.me/+972547401660?text=${getCartDetails(cartItems , totalCost)}`} 
+            to={`https://wa.me/+972547401660?text=${getCartDetails(cartItems , totalCost, isMehadrin)}`} 
             target='_blank'
             style={{ textDecoration: 'none' }}> 
             <Button variant="contained" color="primary" disableElevation>
