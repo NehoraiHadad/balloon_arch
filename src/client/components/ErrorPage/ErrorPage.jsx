92b4080cc030cb749361df90fa6b4732ca5d0bc6
Nehorai Hadad <Nehorai.Hadad@gmail.com>
2023-05-03 19:19:42 +0300

    fix font and add button for link

--- src/client/components/ErrorPage/ErrorPage.jsx
+++ src/client/components/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,30 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import Images from "../Images/Images";
 
 const ErrorPage = () => {
   return (
-    <Grid container dir={"rtl"} justifyContent={"center"} textAlign={"center"} alignItems={"center"} width={"100vw"} height={"100vh"}>
-        <Grid item width={"100%"}>
-          <Typography fontSize={"2.5rem"} color={"red"}> יש לנו שגיאה! :( </Typography>
-          <Link to={"/"} style={{textDecoration: "none"}}> בואו נחזור להתחלה... </Link>
-        </Grid>
-        <Grid item>
-          <Typography component={"img"} src={Images.ErorrImg} width={"35rem"} />
+    <Grid
+      container
+      dir={"rtl"}
+      justifyContent={"center"}
+      textAlign={"center"}
+      alignItems={"center"}
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+    >
+      <Grid item width={"100%"} marginBottom={3}>
+        <Typography fontSize={"2.5rem"} color={"red"}>
+          {" "}
+          יש לנו שגיאה! :({" "}
+        </Typography>
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <Button sx={{ fontSize: "1.3rem" }}>בואו נחזור להתחלה... </Button>
+        </Link>
+      </Grid>
+      <Grid item>
+        <Typography component={"img"} src={Images.ErorrImg} width={"35rem"} />
       </Grid>
     </Grid>
   );
